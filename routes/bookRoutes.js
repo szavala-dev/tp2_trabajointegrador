2ec27f6266fe8e9289e2d48af5dab00725f265c7
Christian <b.christian.fuentes@gmail.com>
2025-06-24 00:47:33 -0300

    aq

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getBooks, getBookById } from '../controllers/BookController.js';
+import { getBooks, getBookById, getAllAuthors } from '../controllers/BookController.js';
 
 const router = express.Router();
 
 router.get('/', getBooks);         // GET /api/books
+router.get(':author:genre', getBooks); // GET /api/books?author=nombre&genre=nombre
 router.get('/:id', getBookById);   // GET /api/books/:id
+router.get('/authors', getAllAuthors); // GET /api/books/authors
 
 export default router;
